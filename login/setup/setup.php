db91dc0861fedc393d6423d37da0f197118a9620
Mell-vin <lgumede@student@wethinkcode.co.za>
2019-10-30 01:45:48 +0200

    30 Oct

--- login/setup/setup.php
+++ login/setup/setup.php
@@ -1,9 +1,9 @@
+#!/C/PHP7/php
 <?php
     require 'database.php';
 
     try {
         $conn = new PDO ("mysql:host=$DB_host", $DB_host, $DB_pwd);
-        //setting the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = "CREATE DATABASE $DB_name";
         $sql->exec($sql);
@@ -20,13 +20,15 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         //lets create our table yea
-        $sql ="CREATE TABLE $Usertable (
+        $sql = "CREATE TABLE CamUsers (
             id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
             Username VARCHAR(25) NOT NULL,
             email VARCHAR(60) NOT NULL,
             Userid VARCHAR(15) NOT NULL,
-            password VARCHAR(64) NOT NULL
+            pwd VARCHAR(60) NOT NULL
             );"
+            $sql->exec($sql);
+            echo "table CamUsers created successfully";
     }
     catch (PDOException $e){
         echo $sql."<br>". $e->getMessage();
@@ -41,9 +43,11 @@
         $sql = "CREATE TABLE gallery (
             id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
             img IMAGE NOT NULL,
-            galid int (10) NOT NULL'
+            galid int (10) NOT NULL,
             FOREIGN KEY (galid) REFERENCES $Usertable(id)
         );"
+        $sql->exec($sql);
+        echo "table gallery created successfully";
     }
     catch (PDOException $e){
         echo $sql."<br>". $e->getMessage();
@@ -57,6 +61,36 @@
 
         $sql = "CREATE TABLE comments (
             id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            userid int(10) NOT NULL,
+            galid int(10) NOT NULL,
+            FOREIGN KEY (userid) REFERENCES $Usertable(id),
+            FOREIGN KEY (galid) REFERENCES gallery(id)
         );"
+        $sql->exec($sql);
+        echo "table comments created successfully";
+    }
+    catch (PDOException $e){
+        echo $sql."<br>". $e->getMessage();
+        exit();
+    }
+
+    try {
+        $conn = new PDO ("mysql:host=$DB_host;dbname=$DB_name", $DB_host, $DB_pwd);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "CREATE TABLE likes (
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            userid int(10) NOT NULL,
+            galid int(10) NOT NULL,
+            liked VARCHAR(1) NOT NULL,
+            FOREIGN KEY (userid) REFERENCES $Usertable(id),
+            FOREIGN KEY (galid) REFERENCES gallery(id)
+        );"
+        $sql->exec($sql);
+        echo "table likes created successfully";
+    }
+    catch (PDOException $e){
+        echo $sql."<br>". $e->getMessage();
+        exit();
     }
 ?>
