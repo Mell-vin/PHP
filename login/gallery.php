963fb03a6d147a760f7f4db998d2a70bc0f52aad
Mell-vin <lgumede@student@wethinkcode.co.za>
2019-11-18 07:34:23 +0200

    18 Nov

--- login/gallery.php
+++ login/gallery.php
@@ -12,38 +12,53 @@
               <button id="shoot">Capture</button>
         </div>
         <canvas id="cam"></canvas>
+        <canvas id="upload"></canvas>
         <div class="output">
             <img id="photo" alt="Waiting for picture... "> 
         </div>
+        <div class="select">
+      			<img class="thumbnail" src="img/gun.png"></img>
+      			<input id="cadre.png" type="radio" name="img" value="./img/cadre.png" onclick="onCheckBoxChecked(this)">
+      			<img class="thumbnail" src="img/cig.png"></img>
+      			<input id="cigarette.png" type="radio" name="img" value="./img/cigarette.png" onclick="onCheckBoxChecked(this)">
+      			<img class="thumbnail" src="img/hat.png"></img>
+      			<input id="hat.png" type="radio" name="img" value="./img/hat.png" onclick="onCheckBoxChecked(this)">
+        </div>
+            <img id="hat" style="display:inline;" src="img/hat.png"></img>
+            <img id="cigarette" style="display:inline;" src="img/cig.png"></img>
+            <img id="gun" style="display:inline;" src="img/gun.png"></img>
         <button id="web" onclick="swap()">webcam</button>
         <button id="saveFunc" onclick="saveFunc()">upload</button>
         <script>
               function swap() {
-                document.getElementById("canvas").style.zIndex = "-1";
-                document.getElementById("cam").style.zIndex = "0";
+                document.getElementById("upload").style.zIndex = "-1";
+                document.getElementById("photo").style.zIndex = "0";
+                var c = document.getElementById("upload");
+                var ctx = c.getContext("2d");
+                ctx.clearRect(0, 0, 800, 800);
               }
 
               function swap2() {
-                document.getElementById("canvas").style.zIndex = "0";
-                document.getElementById("cam").style.zIndex = "-1";
+                document.getElementById("upload").style.zIndex = "0";
+                document.getElementById("photo").style.zIndex = "-1";
               }
 
               document.getElementById('browse').onchange = function(e) {
-  var img = new Image();
-  img.onload = draw;
-  img.onerror = failed;
-  img.src = URL.createObjectURL(this.files[0]);
-};
-function draw() {
-  var canvas = document.getElementById('canvas');
-  canvas.width = this.width;
-  canvas.height = this.height;
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(this, 0,0);
-}
-function failed() {
-  console.error("The provided file couldn't be loaded as an Image media");
-}
+              var img = new Image();
+              img.onload = draw;
+              img.onerror = failed;
+              img.src = URL.createObjectURL(this.files[0]);
+              };
+              function draw() {
+              var canvas = document.getElementById('upload');
+              canvas.width = this.width;
+              canvas.height = this.height;
+              var ctx = canvas.getContext('2d');
+              ctx.drawImage(this, 0,0);
+              }
+              function failed() {
+              console.error("The provided file couldn't be loaded as an Image media");
+              }
         </script>
         <script type="text/javascript" src="java/cam2.js"></script>
     </body>
